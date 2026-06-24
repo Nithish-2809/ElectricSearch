eba043b35bb41bf92d7c5d91b47db71b383446ac
Nithish Reddy <nithishreddy2809@gmail.com>
2026-06-24 19:43:05 +0530

    added task to each worker

--- electron/workers/WorkerPool.js
+++ electron/workers/WorkerPool.js
@@ -3,26 +3,40 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { Worker } from "node:worker_threads";
 
-class WorkerPool {
-    constructor() {
-        this.workerCount = Math.max(1, os.cpus().length - 1);
+export class WorkerPool {
+  constructor() {
+    this.workerCount = Math.max(1, os.cpus().length - 1);
 
-        this.workers = [];
-        this.idleWorkers = [];
-        this.queue = [];
+    this.workers = [];
+    this.idleWorkers = [];
+    this.queue = [];
 
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-        const workerPath = path.join(__dirname, "OCRWorker.js");
+    const workerPath = path.join(__dirname, "OCRWorker.js");
 
-        for (let i = 0; i < this.workerCount; i++) {
-            const worker = new Worker(workerPath);
+    for (let i = 0; i < this.workerCount; i++) {
+      const worker = new Worker(workerPath);
 
-            this.workers.push(worker);
-            this.idleWorkers.push(worker);
+      worker.on("message", (result) => {
+        this.idleWorkers.push(worker);
+
+        this.processQueue();
+      });
+
+      worker.on("error", (error) => {
+        console.error("Worker Error:", error);
+      });
+
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          console.error(`Worker exited with code ${code}`);
         }
-    }
-}
+      });
 
-export default WorkerPool;
+      this.workers.push(worker);
+      this.idleWorkers.push(worker);
+    }
+  }
+}
