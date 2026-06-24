3f1559984c78e0c9b544898aa64a8e8a369fccdc
Nithish Reddy <nithishreddy2809@gmail.com>
2026-06-24 20:11:46 +0530

    refractored OCRWorker by removing saveText task from it

--- electron/workers/OCRWorker.js
+++ electron/workers/OCRWorker.js
@@ -1,17 +1,14 @@
-import { parentPort } from "worker_threads";
-
-import OCRService from "../services/OCRService.js";
-import IndexRepository from "../database/IndexRepository.js";
+import { parentPort } from "node:worker_threads";
+import { extractText } from "../services/OCRService.js";
 
 parentPort.on("message", async (image) => {
     try {
-        const ocrText = await OCRService.extractText(image.path);
-
-        await IndexRepository.saveOCRText(image.id, ocrText);
+        const ocrText = await extractText(image.path);
 
         parentPort.postMessage({
             success: true,
             imageId: image.id,
+            ocrText,
         });
     } catch (error) {
         parentPort.postMessage({
