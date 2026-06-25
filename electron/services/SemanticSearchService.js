d37e678f5c345a01fab1d922cf68b561d57eeb86
Nithish Reddy <nithishreddy2809@gmail.com>
2026-06-25 20:41:22 +0530

    added semantic search service

---
+++ electron/services/SemanticSearchService.js
@@ -0,0 +1,29 @@
+import { generateEmbedding } from "./EmbeddingService.js";
+import { getAllEmbeddings } from "../database/IndexRepository.js";
+import { cosineSimilarity } from "../utils/similarity.js";
+
+export async function semanticSearch(query) {
+    const queryEmbedding = await generateEmbedding(query);
+
+    const images = await getAllEmbeddings();
+
+    const results = [];
+
+    for (const image of images) {
+        const embedding = JSON.parse(image.embedding);
+
+        const score = cosineSimilarity(
+            queryEmbedding,
+            embedding
+        );
+
+        results.push({
+            ...image,
+            score,
+        });
+    }
+
+    results.sort((a, b) => b.score - a.score);
+
+    return results.slice(0, 25);
+}
